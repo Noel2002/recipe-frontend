3fef728392e52004b06c4e67f903b76974e65331
Noel2002 <noel@Berwas-Mac-mini.local>
2023-12-26 16:48:02 +0800

    restyled chef-word section; added real description  to featured video

--- src/views/Home/ChefWord/ChefWord.js
+++ src/views/Home/ChefWord/ChefWord.js
@@ -7,17 +7,39 @@ const ChefWord = () => {
   return (
     <Section className="bg-primary-light">
         <Title>
-            Chef's word
+            About the chef
         </Title>
-        <div className={`${styles.ellipse}`}>
-            <img src="https://res.cloudinary.com/nowo-ltd/image/upload/v1619865701/ARTISTA/back1_itcz7c.jpg" />
+        <div className={styles["root"]}>
+            <div className={styles["left"]}>
+                <h3 className={styles['name-plug']}>Hi, I am Amanda Jones</h3>
+                <div className={styles['summary']}>
+                    <p>Welcome to Amanda Jones' recipe website, your go-to 
+                        destination for mouthwatering dishes and culinary 
+                        inspiration. Amanda is a passionate home cook and 
+                        recipe developer with a love for creating delicious 
+                        meals that bring people together. With a diverse 
+                        collection of recipes spanning various cuisines and 
+                        dietary preferences, Amanda's goal is to help you 
+                        explore the joy of cooking and expand your culinary 
+                        horizons.
+                    </p>
+                    <p>Amanda's culinary journey began at a young age, watching 
+                        her grandmother whip up traditional family recipes with 
+                        love and care. Inspired by these cherished memories and 
+                        armed with a desire to share her love for cooking, 
+                        Amanda started her recipe blog to connect with fellow 
+                        food enthusiasts and provide accessible recipes for 
+                        home cooks of all skill levels. Whether you're a 
+                        seasoned chef or just starting your culinary adventure, 
+                        Amanda's recipes are thoughtfully crafted, easy to 
+                        follow, and guaranteed to tantalize your taste buds.
+                    </p>
+                </div>
+            </div>
+            <div className={styles['right']}>
+                <img src='https://res.cloudinary.com/nowo-ltd/image/upload/v1703578398/recipes/istockphoto-1398325952-612x612_gvp0rq.jpg' alt='author profile image' />
+            </div>
         </div>
-        <p className={`${styles.word} paragraph mt-20`}>
-            Lorem ipsum dolor sit amet. A laborum enim et internos maiores vel voluptas omnis. Qui error enim qui consequatur expedita et quia quos ut internos officiis aut totam illum eos repellendus harum. Et doloremque libero 33 facere minus ut quos suscipit.
-        </p>
-        <p className={`${styles["name-tag"]} mt-20`}>
-            - Chef. John Doe
-        </p>
     </Section>
   )
 }
--- src/views/Home/FeaturedVideo/FeaturedVideo.js
+++ src/views/Home/FeaturedVideo/FeaturedVideo.js
@@ -4,6 +4,7 @@ import styles from './featuredVideo.module.css';
 import CardMedia from '../../../components/Cards/Card/CardMedia/CardMedia';
 import CardHeader from '../../../components/Cards/Card/CardHeader/CardHeader';
 import Title from '../../../components/Title/Title';
+import { Link } from 'react-router-dom';
 
 const FeaturedVideo = () => {
   return (
@@ -26,11 +27,17 @@ const FeaturedVideo = () => {
                     How to Create a Healthy Plate With Richard Smith
                 </CardHeader>
                 <p className={`paragraph mt-10`}>
-                    Lorem ipsum dolor sit amet. Qui odio placeat qui inventore officiis ea cupiditate dolores. Ea iure suscipit qui perspiciatis quos quo doloribus aperiam
+                    In this informative video, nutritionist and wellness expert 
+                    Richard Smith shares his valuable insights on creating a 
+                    healthy plate. Join him as he guides you through the 
+                    process of selecting the right combination of foods to 
+                    optimize your health. 
                 </p>
-                <button className={`${styles.btn} mt-20`}>
-                    watch video
-                </button>
+                <Link to='https://www.youtube.com/watch?v=pQrchxj2gC8' target='_blank'>
+                    <button className={`${styles.btn} mt-20`}>
+                        watch video
+                    </button>
+                </Link>
             </div>
            
         </div>
